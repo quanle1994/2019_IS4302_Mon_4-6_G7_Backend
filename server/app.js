437033ddf640d15d0ebbd29a5eff54c1d692b39c
quanle1994 <quan_le@u.nus.edu>
2019-03-25 07:45:42 +0800

    add file saving end point

--- server/app.js
+++ server/app.js
@@ -1,14 +1,39 @@
 require('dotenv').load();
 
-var express = require('express');
-var proxy = require('express-http-proxy');
-var jwt = require('jsonwebtoken');
+const express = require('express');
+const proxy = require('express-http-proxy');
+const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const rand = require("randomstring");
+const app = new express();
 
-var app = new express();
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        console.log(req);
+        cb(null, process.env.PHOTO_DIR);
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${rand.generate(10)}${file.originalname}`)
+    }
+});
+const upload = multer({
+    storage: storage,
+});
+app.use(express.static('static'));
+app.use('hlf', proxy('localhost:3001/'));
+app.use('alf', proxy('localhost:3002/'));
+app.use('blf', proxy('localhost:3003/'));
+app.use('clf', proxy('localhost:3004/'));
 
-app.use(express.static('static'))
-app.use('hlf', proxy('localhost:3001/'))
+app.get('/getPhoto', (req, res) => {
+    res.sendFile(process.env.PHOTO_DIR + '/test.jpg');
+});
 
-app.listen(process.env.NODE_PORT, '0.0.0.0', () => {
+app.get('/savePhoto', upload.single('photo'), (req, res) => {
+    console.log(req.file);
+    res.status(200).send();
+});
+
+app.listen(process.env.NODE_PORT, () => {
     console.log(`Server started on port: ${process.env.NODE_PORT}`)
 });
