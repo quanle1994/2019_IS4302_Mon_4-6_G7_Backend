27e495680b10102004a657bb3cc4273f73fc18c4
quanle1994 <quan_le@u.nus.edu>
2019-03-26 05:19:07 +0800

    fabricate more data

--- server/models/fakeModels.js
+++ server/models/fakeModels.js
@@ -4,13 +4,17 @@ const poheng = {
   assets:{
     golds: {
       balance: [
-        {weight: 12345, purity: 99.9, cost: 1234500},
-        {weight: 123, purity: 98.9, cost: 12345}
+        {id: 1, weight: 12345, purity: 99.9, cost: 1234500},
+        {id: 2, weight: 123, purity: 98.9, cost: 12345},
+        {id: 3, weight: 1233, purity: 95.9, cost: 123345}
       ],
       transactions: [],
     },
     deeds: {
-      balance: [],
+      balance: [
+        {maxWeight: 500, purity: 99.9, unitPrice: 2000, status: 'FOR_SALE', goldId: 1},
+        {maxWeight: 500, purity: 98.9, unitPrice: 1000, status: 'INTERNAL', goldId: 2},
+      ],
       transactions: [],
     },
     money: {
